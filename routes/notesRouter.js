daa8ebe2190fb399d11b70d41c76b6951f71ceb3
Tushar Soni <71227235+TusharSoni014@users.noreply.github.com>
2023-12-11 15:43:33 +0530

    added update note route

--- routes/notesRouter.js
+++ routes/notesRouter.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { createNote, deleteNote } = require("../controllers/notesController");
+const {
+  createNote,
+  deleteNote,
+  updateNote,
+} = require("../controllers/notesController");
 const verifyToken = require("../middlewares/verifyToken");
 const notesRouter = express.Router();
 
 notesRouter.post("/create", verifyToken, createNote);
 notesRouter.delete("/delete/:id", verifyToken, deleteNote);
+notesRouter.put("/update/:id", verifyToken, updateNote);
 
 module.exports = notesRouter;
